846cffee2eec25a4cfdf1da476cc22736220a05b
yatharthK <yatharth.kapadia.1702@gmail.com>
2021-09-24 01:06:28 +0530

    managed seperate eval file

--- eval.py
+++ eval.py
@@ -3,6 +3,7 @@ from generator import Generator
 from discriminator import Discriminator
 from dataset import MapDataset
 from torch.utils.data import DataLoader
+import torch.optim as optim
 
 
 
@@ -11,6 +12,8 @@ disc = Discriminator(in_channels=3).to(config.DEVICE)
 gen = Generator(in_channels=3, features=64).to(config.DEVICE)
 val_dataset = MapDataset(root_dir=config.VAL_DIR)
 val_loader = DataLoader(val_dataset, batch_size=1, shuffle=False)
+opt_disc = optim.Adam(disc.parameters(), lr=config.LEARNING_RATE, betas=(0.5, 0.999),)
+opt_gen = optim.Adam(gen.parameters(), lr=config.LEARNING_RATE, betas=(0.5, 0.999))
 
 if config.LOAD_MODEL:
         load_checkpoint(
